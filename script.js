7ffc3b8bd87b7625720ac16f12e16c076da5eb5f
Sampada Santosh Joshi <71493907+Samapadajoshi@users.noreply.github.com>
2025-03-19 13:38:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const todoInput = document.getElementById("todo-input");
 const todosParent = document.getElementById("todos-wrapper");
 
+const todos = [];
+
+
 function addTodo() {
     const todoValue = todoInput.value;
 
@@ -10,13 +13,24 @@ function addTodo() {
         return;
     }
 
-    // creating para element
-    const todoPara = document.createElement("p");
-
-    todoPara.innerText = todoValue;
-
-    // adding element to front end
-    todosParent.appendChild(todoPara);
+    todos.push(todoValue);
+    //magic box --> todo -----> frontend 
+    displayTodos();
 
     todoInput.value = "";
+}
+
+function displayTodos() {
+    //todos --> ??
+    todosParent.innerHTML = "";
+    for (let i = 0; i < todos.length; i++) {
+        // creating para element
+        const todoPara = document.createElement("p");
+
+        todoPara.innerText = todoValue;
+
+        // adding element to front end
+        todosParent.appendChild(todoPara);
+    }
+
 }
